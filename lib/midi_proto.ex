e76e2ff0edbff8fda2477fe4e738357cf1b68f13
James Harton <james@automat.nz>
2020-05-05 15:41:36 +1200

    Empty mix app.

---
+++ lib/midi_proto.ex
@@ -0,0 +1,18 @@
+defmodule MidiProto do
+  @moduledoc """
+  Documentation for `MidiProto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MidiProto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/midi_proto_test.exs
@@ -0,0 +1,8 @@
+defmodule MidiProtoTest do
+  use ExUnit.Case
+  doctest MidiProto
+
+  test "greets the world" do
+    assert MidiProto.hello() == :world
+  end
+end
